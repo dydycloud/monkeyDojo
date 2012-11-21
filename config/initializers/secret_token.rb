a746ff72be2a701259c1792c2b6382803ae78c3d
bemonkey <bemonkey@ubuntu.(none)>
2012-11-21 15:37:13 +0100

    first commit product scaffold and css

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shop::Application.config.secret_token = '549dcbce73323845a97c57f8f933216ab6d17bdef6c9fb0aa35b96dbd32dfe13d02a0cd8ad39803215eea69b9f229db68f777a89466d12c465a02225e45b4e1c'
