6dabebe1387fb5c9089ec6567244bc91144eb7d3
dydycloud <sandy.pierre97@gmail.com>
2012-12-06 15:55:48 +0100

    set up basic mail sender

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,24 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "Monkey Dojo<monkeydojo@example.com>"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: "Monkey Dojo Order Confirmation"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+    mail to: order.email, subject: 'Monkey Dojo Order Shipped'
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,19 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Shop::Application.configure do
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+		address:		"smtp.gmail.com",
+		port:		587,
+		domain:		"",
+		authentication: "plain",
+		user_name:		"sandy.pierre97@gmail.com",
+		password:		"epicure971",
+		enable_starttls_auto: true
+	}
+end
+
+
 # Initialize the rails application
 Shop::Application.initialize!
