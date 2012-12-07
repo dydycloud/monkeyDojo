3d5227c9969d55b2d3ef75f72e273761ad4a43ee
dydycloud <sandy.pierre97@gmail.com>
2012-12-07 12:12:46 +0100

    add user login logout session admin part

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Shop::Application.routes.draw do
+  get "admin/index"
+
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
+  resources :users
+
+
   resources :orders
 
 
